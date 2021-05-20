6bf49c21b1a7b7c1ff2c24a4187c5bc8534dfbc6
9juandromero7 <82004923+9juandromero7@users.noreply.github.com>
2021-05-20 18:48:41 -0500

    Add files via upload

---
+++ Nombre, phyton-turtle.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 20 18:43:52 2021
+
+@author: Juan Diego
+"""
+
+import turtle
+tortuguita = turtle.Turtle()
+colors=['red', 'purple', 'blue', 'green', 'yellow', 'orange', 'pink','light blue']
+
+"J"
+tortuguita.color("blue")
+tortuguita.forward(50)
+tortuguita.left(270)
+tortuguita.forward(100)
+tortuguita.left(270)
+tortuguita.forward(40)
+tortuguita.left(270)
+tortuguita.forward(20)
+tortuguita.left(270)
+tortuguita.forward(70)
+"u"
+tortuguita.color("red")
+tortuguita.left(90)
+tortuguita.forward(60)
+tortuguita.left(180)
+tortuguita.forward(80)
+tortuguita.left(90)
+tortuguita.forward(40)
+tortuguita.left(90)
+tortuguita.forward(80)
+tortuguita.left(180)
+tortuguita.forward(80)
+tortuguita.left(90)
+tortuguita.forward(20)
+"a"
+tortuguita.color("green")
+tortuguita.left(90)
+tortuguita.forward(80)
+tortuguita.left(270)
+tortuguita.forward(40)
+tortuguita.left(270)
+tortuguita.forward(70)
+tortuguita.left(270)
+tortuguita.forward(40)
+tortuguita.left(180)
+tortuguita.forward(60)
+"n"
+tortuguita.color("purple")
+tortuguita.left(270)
+tortuguita.forward(10)
+tortuguita.left(180)
+tortuguita.forward(80)
+tortuguita.left(270)
+tortuguita.forward(40)
+tortuguita.left(270)
+tortuguita.forward(80)
+"rashito"
+tortuguita.color("yellow")
+tortuguita.left(130)
+tortuguita.forward(40)
+tortuguita.left(230)
+tortuguita.forward(20)
+tortuguita.left(130)
+tortuguita.forward(40)
